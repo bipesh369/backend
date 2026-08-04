0950c1f12b68d2c48c877f5a7affe8752c92f6c2
bipesh369 <juniorbipesh@gmail.com>
2026-08-04 16:12:03 +0000

    Convert 4.project to a normal directory

---
+++ 4.project/src/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv"
+
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path: '../.env'
+})
+
+connectDB()
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+
+( async () => {
+   try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DBB_NAME}`)
+
+      app.on("error", (error) => {
+        console.log("ERRR: ", error)
+      })
+      app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`)
+      })
+
+   } catch (error) {
+      console.log("ERROR: ", error)
+      throw err
+   }
+})
+   */
