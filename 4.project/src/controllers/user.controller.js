d620af661288beec0d5db118903901be3165aa43
bipesh369 <juniorbipesh@gmail.com>
2026-08-11 17:31:54 +0000

    add logoutUser

--- 4.project/src/controllers/user.controller.js
+++ 4.project/src/controllers/user.controller.js
@@ -159,6 +159,17 @@ const logoutUser = asyncHandler(async(req, res) => {
       new: ture
     }
   )
+
+  const options = {
+      httpOnly: true,
+      secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out"))
 })
 
 
