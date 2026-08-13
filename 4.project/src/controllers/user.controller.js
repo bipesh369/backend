f4e02a4e3455e9ecda9b4b0f7ed95a9c0b7a7a06
bipesh369 <juniorbipesh@gmail.com>
2026-08-13 08:49:42 +0000

    feat: add updateUserAvatar and updateCoverImage functionalities

--- 4.project/src/controllers/user.controller.js
+++ 4.project/src/controllers/user.controller.js
@@ -280,6 +280,63 @@ const updateAccountDetails = asyncHandler(async(req, res) =>{
   .json(new ApiResponse(200, user, "Account details updated successfully"))
 })
 
+const updateUserAvatar = asyncHandler(async(req, res) =>{
+   const avatarLocalPath = req.file?.path
+
+   if(!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is misssing");
+   }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+   if(!avatar.url) {
+    throw new Error(400, "Error while uploading on avatar");
+   }
+
+   const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url
+      }
+    },
+    {new: true}
+   ).select("-password")
+   
+   return res
+  .status(200)
+  .json(new ApiResponse(200, user, "Avatar image updated successfully"))
+})
+
+const updateCoverImage = asyncHandler(async(req, res) =>{
+   const coverImageLocalPath = req.file?.path
+
+   if(!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image file is misssing");
+   }
+
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!coverImage.url) {
+    throw new Error(400, "Error while uploading on cover image");
+   }
+
+   const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    },
+    {new: true}
+   ).select("-password")
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, "Cover image updated successfully"))
+})
+
+
 
 // export default registerUser
 // export default loginUser
@@ -291,5 +348,7 @@ export default {
   refreshAccessToken,
   changeCurrentPassword,
   getCurrentUser,
-  updateAccountDetails
+  updateAccountDetails,
+  updateUserAvatar,
+  updateCoverImage
 }
