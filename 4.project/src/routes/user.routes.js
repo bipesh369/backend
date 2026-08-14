e278884a5c42201cb18752a1dd996a16dbd47036
bipesh369 <juniorbipesh@gmail.com>
2026-08-14 15:28:26 +0000

    feat: add change password functionality to user routes

--- 4.project/src/routes/user.routes.js
+++ 4.project/src/routes/user.routes.js
@@ -3,6 +3,7 @@ import userController from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import refreshAccessToken from "../controllers/user.controller.js"
+import changeCurrentPassword from "../controllers/user.controller.js"
 
 const router = Router();
 
@@ -17,20 +18,45 @@ router.route("/register").post(
             maxCount: 1
         }
     ]),
-    userController.registerUser
-);
+    userController.registerUser)
 
 router.route("/login").post(
-    userController.loginUser
-);
+    userController.loginUser)
 
 router.route("/logout").post(
     verifyJWT,
-    userController.logoutUser
-);
+    userController.logoutUser)
 
 router.route("/refresh-token").post(
-    userController.refreshAccessToken
-)
+    userController.refreshAccessToken)
+
+router.route("/change-password").post(
+    verifyJWT, 
+    userController.changeCurrentPassword)
+
+router.route("/current-user").get(
+    verifyJWT, 
+    userController.getCurrentUser)
+
+router.route("/update-account").patch(
+    verifyJWT, 
+    userController.updateAccountDetails)
+
+router.route("/avatar").patch(
+    verifyJWT, upload.single("avatar"), 
+    userController.updateUserAvatar)
+
+router.route("/cover-image").patch(
+    verifyJWT, upload.single("coverImage"),
+    userController.updateCoverImage)
+
+router.route("/c/:username").get(
+    verifyJWT, 
+    userController.getUserChannelProfile)    
+
+router.route("/watch-history").get(
+    verifyJWT, 
+    userController.getWatchhistory)    
+
 
 export default router;
